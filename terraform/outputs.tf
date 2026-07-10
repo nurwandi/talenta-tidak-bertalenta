64e91e0a8e51c3fc17e74de62b207c42699515e7
Obi <qolbinurwandi@gmail.com>
2026-07-11 02:38:09 +0700

    feat(tf): outputs for ECR url and lambda names

---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "ecr_repository_url" {
+  value = aws_ecr_repository.this.repository_url
+}
+
+output "lambda_function_names" {
+  value = [for f in aws_lambda_function.clock : f.function_name]
+}
